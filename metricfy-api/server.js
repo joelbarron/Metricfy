9810bcda6c181c5f8846a43a5a8053ebd6c96759
Joel Barron <jbarron_web@hotmail.com>
2018-04-19 22:00:40 -0500

    se corrigieron errores en el modulo

--- metricfy-api/server.js
+++ metricfy-api/server.js
@@ -4,12 +4,10 @@ const http = require('http')
 const express = require('express')
 const asyncify = require('express-asyncify')
 const chalk = require('chalk')
-const debug = require('debug')('metricfy:api')
-const { handleFatalError } = require('metricfy-utils')
+const { handleFatalError, errorHandlerExpress } = require('metricfy-utils')
+const { configApi } = require('metricfy-config')
 
 const apiv1 = require('./api-v1')
-
-const port = process.env.PORT || 3000
 
 // const app = express()
 const app = asyncify(express()) // solucion al async await para express, ya que nativamente no lo soporta
@@ -19,28 +17,15 @@ const server = http.createServer(app)
 app.use('/v1', apiv1)
 
 // Express error handled
-app.use((err, req, res, next) => {
-  debug(`Error: ${err.message}`)
-
-  // Response with the error if has a status property
-  if (err.status) {
-    return res.status(err.status).send(err)
-  }
-
-  if (err.message.match(/not found/)) {
-    return res.status(404).send({ error: err.message })
-  }
-
-  res.status(500).send({ error: err.message })
-})
+app.use(errorHandlerExpress)
 
 if (!module.parent) {
   // lanzar el servidor
   process.on('uncaughtExeption', handleFatalError)
   process.on('unhandledRejection', handleFatalError)
 
-  server.listen(port, () => {
-    console.log(`${chalk.green('[metricfy-api]')} server listening on port ${port}`)
+  server.listen(configApi.port, () => {
+    console.log(`${chalk.green('[metricfy-api]')} server listening on port ${configApi.port}`)
   })
 }
 
