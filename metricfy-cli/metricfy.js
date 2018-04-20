708672070d3e55646658d6b5abb6c5ec4c216426
Joel Barron <jbarron_web@hotmail.com>
2018-04-20 09:46:36 -0500

    se termino el modulo de cli, esta listo para poner el proyecto en produccion

--- metricfy-cli/metricfy.js
+++ metricfy-cli/metricfy.js
@@ -4,10 +4,21 @@
 
 /* eslint new-cap: "off" */
 
+const moment = require('moment')
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const MetricfyAgent = require('metricfy-agent')
 
+const agent = new MetricfyAgent()
 const screen = blessed.screen()
+
+const agents = new Map()
+const agentsMetrics = new Map()
+let extended = []
+let selected = {
+  uuid: null,
+  type: null
+}
 
 const grid = new contrib.grid({
   rows: 1,
@@ -26,8 +37,136 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentsMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+agent.on('agent/disconnected', payload => {
+  const { uuid } = payload.agent
+
+  if (agents.has(uuid)) {
+    agents.delete(uuid)
+    agentsMetrics.set(uuid)
+  }
+
+  renderData()
+})
+
+agent.on('agent/message', payload => {
+  const { uuid } = payload.agent
+  const { timestamp } = payload
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentsMetrics.set(uuid, {})
+  }
+
+  const metrics = agentsMetrics.get(uuid)
+
+  payload.metrics.forEach(m => {
+    const { type, value } = m
+
+    if (!Array.isArray(metrics[type])) {
+      metrics[type] = []
+    }
+
+    const length = metrics[type].length
+
+    if (length >= 20) {
+      metrics[type].shift()
+    }
+
+    metrics[type].push({
+      value,
+      timestamp: moment(timestamp).format('HH:mm:ss')
+    })
+  })
+
+  renderMetric()
+})
+
+tree.on('select', node => {
+  const { uuid } = node
+
+  if (node.agent) {
+    node.extended ? extended.push(uuid) : extended = extended.filter(e => e !== uuid)
+    selected.uuid = null
+    selected.type = null
+    return
+  }
+
+  selected.uuid = uuid
+  selected.type = node.type
+  renderMetric()
+})
+
+function renderData () {
+  const treeData = {}
+
+  let idx = 0
+
+  for (let [ uuid, val ] of agents) {
+    const title = `${val.name} - (${val.pid})`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      extended: extended.includes(uuid),
+      children: {}
+    }
+
+    const metrics = agentsMetrics.get(uuid)
+
+    Object.keys(metrics).forEach(type => {
+      const metric = {
+        uuid,
+        type,
+        metric: true
+      }
+
+      const metricName = ` ${type}  ${' '.repeat(1000)} ${idx}`
+      treeData[title].children[metricName] = metric
+      idx++
+    })
+  }
+
+  tree.setData({
+    extended: true,
+    children: treeData
+  })
+
+  screen.render()
+}
+
+function renderMetric () {
+  if (!selected.uuid && !selected.type) {
+    line.setData([{ x: [], y: [], title: '' }])
+    screen.render()
+    return
+  }
+
+  const metrics = agentsMetrics.get(selected.uuid)
+  const values = metrics[selected.type]
+  const series = [{
+    title: selected.type,
+    x: values.map(v => v.timestamp).slice(-10),
+    y: values.map(v => v.value).slice(-10)
+  }]
+
+  line.setData(series)
+  screen.render()
+}
+
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
+tree.focus()
 screen.render()
